28bd6f53ddf530d25e68df4bfb4b634be0cda4c2
Lucia_Gonzalez <Lucia_Gonzalez@ITRP1107>
2024-02-15 15:33:06 -0300

    Add project files.

---
+++ HomeBankingMindHub/Models/DBInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"},
+                    new Client { Email = "marcosGG@gmail.com", FirstName="Marcos", LastName="Gonzalez", Password="678910"},
+                    new Client { Email = "veronuñez@gmail.com", FirstName="Veronica", LastName="Nuñez", Password="345623"}
+                };
+
+                context.Clients.AddRange(clients);
+
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ HomeBankingMindHub/Program.cs
@@ -0,0 +1,53 @@
+using HomeBankingMindHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<HomeBankingContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion")));
+
+// Add services to the container.
+
+//builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+
+    //Aqui obtenemos todos los services registrados en la App
+    var services = scope.ServiceProvider;
+    try
+    {
+
+        // En este paso buscamos un service que este con la clase HomeBankingContext
+        var context = services.GetRequiredService<HomeBankingContext>();
+        DBInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ha ocurrido un error al enviar la información a la base de datos!");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
